dff384105ba7c2c9b48757ca1fb16e9c7c175b36
abbas80 <dehghanzadehabbas@gmail.com>
2025-08-23 11:35:22 +0330

    make `Usermodel` as default AUTH_MODEL to prevent error!

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -84,8 +84,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'accounts.UserModel'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
