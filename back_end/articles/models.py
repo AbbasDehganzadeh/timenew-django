68cedda65a50dfeeacf70386910ae24edfda87f1
abbas80 <dehghanzadehabbas@gmail.com>
2025-08-27 14:28:49 +0330

    Finish news.models & article.models

--- back_end/articles/models.py
+++ back_end/articles/models.py
@@ -1,26 +1,72 @@
 from django.db import models
 
+from main.models import BaseModel, BaseContentModel
+from categories.models import ContentStatus
 from accounts.models import UserModel
 
-class ArticleModel(models.Model):
-    pass
-    # slug
-    # title: META
-    # excerpt: META
-    # header: CONTENT
-    # body: CONTENT
-    # created_at
-    # updated_at
-    # priority
-    # Pictures: MANY-TO-1
-    # status: (DRAFT, SUBMITTED, APPROVED, PUBLISHED, ARCHIVED)
 
-class BookmarkModel(models.Model):
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    aritcle = models.ForeignKey(ArticleModel, on_delete=models.CASCADE)
-    is_bookmarked = models.BooleanField(default=True)
+class ArticleModel(BaseContentModel):
+    header = models.CharField(max_length=100, verbose_name="عنوان")
+    body = models.TextField(max_length=10000, verbose_name="بدنه‌ی محتوا", blank=True)
+    picture = models.ImageField(
+        upload_to="images/heading_image/", verbose_name="تصویر اصلی"
+    )
+    author = models.ForeignKey(
+        UserModel,
+        verbose_name="نویسنده",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="article_authors",
+        related_query_name="article_author",
+    )
+    status = models.CharField(
+        max_length=2,
+        verbose_name="وضعیت",
+        choices=ContentStatus,
+        default=ContentStatus.DRAFT,
+    )
 
-class CommentModel(models.Model):
-    user = models.ForeignKey(UserModel, null=True, on_delete=models.SET_NULL)
-    aritcle = models.ForeignKey(ArticleModel, null=True, on_delete=models.SET_NULL)
-    text = models.CharField(max_length=200)
+
+class BookmarkModel(BaseModel):
+    user = models.ForeignKey(
+        UserModel,
+        verbose_name="کاربر",
+        on_delete=models.CASCADE,
+        related_name="bookmarks",
+        related_query_name="bookmark",
+    )
+    article = models.ForeignKey(
+        ArticleModel,
+        verbose_name="مقاله",
+        on_delete=models.CASCADE,
+        related_name="bookmarks",
+        related_query_name="bookmark",
+    )
+    is_bookmarked = models.BooleanField(default=True, verbose_name="نشان شده")
+
+
+class CommentModel(BaseModel):
+    user = models.ForeignKey(
+        UserModel, verbose_name="کاربر", null=True, on_delete=models.SET_NULL
+    )
+    aritcle = models.ForeignKey(
+        ArticleModel, verbose_name="مقاله", null=True, on_delete=models.SET_NULL
+    )
+    text = models.CharField(max_length=250, verbose_name="متن", blank=True)
+
+
+class ContributorModel(BaseModel):
+    user = models.ForeignKey(
+        UserModel,
+        verbose_name="کاربر",
+        on_delete=models.CASCADE,
+    )
+    article = models.ForeignKey(
+        ArticleModel,
+        verbose_name="مقاله",
+        on_delete=models.CASCADE,
+    )
+    date_contrib = models.DateTimeField(
+        verbose_name="تاریخ مشارکت", blank=True, null=True,
+    )
+    is_approved = models.BooleanField(default=False, verbose_name="تایید شده")
--- back_end/news/models.py
+++ back_end/news/models.py
@@ -1,21 +1,44 @@
 from django.db import models
 
+from main.models import BaseModel, BaseContentModel
+from categories.models import ContentStatus
 from accounts.models import UserModel
 
-class NewsModel(models.Model):
-    pass
-    # slug
-    # title: META
-    # excerpt: META
-    # header: CONTENT
-    # text: CONTENT
-    # created_at
-    # updated_at
-    # priority
-    # Picture:
-    # status: (DRAFT, SUBMITTED, APPROVED, PUBLISHED, ARCHIVED)
 
-class LikesModel(models.Model):
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    news = models.ForeignKey(NewsModel, on_delete=models.CASCADE)
-    liked = models.BooleanField()
+class TagModel(BaseContentModel):
+    name = models.CharField(max_length=10, verbose_name="نشانک", unique=True)
+    hyped = models.BooleanField(verbose_name="بخار شده", default=False)
+
+class NewsModel(BaseContentModel):
+    header = models.CharField(max_length=100, verbose_name="عنوان")
+    text = models.TextField(max_length=2000, verbose_name="متن خبر", blank=True)
+    picture = models.ImageField(
+        upload_to="images/heading_image/", verbose_name="تصویر اصلی"
+    )
+    tags = models.ManyToManyField(
+        TagModel,
+        verbose_name="تگ‌ها",
+        related_name="news_tags",
+        related_query_name="news_tag",
+        db_table="tag_news",
+    )
+    author = models.ForeignKey(
+        UserModel,
+        verbose_name="نویسنده",
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="news_authors",
+        related_query_name="news_author",
+    )
+    status = models.CharField(
+        max_length=2,
+        verbose_name="وضعیت",
+        choices=ContentStatus,
+        default=ContentStatus.DRAFT,
+    )
+
+
+class LikeModel(BaseModel):
+    user = models.ForeignKey(UserModel, verbose_name="کاربر", on_delete=models.CASCADE)
+    news = models.ForeignKey(NewsModel, verbose_name="خبر", on_delete=models.CASCADE)
+    liked = models.BooleanField(verbose_name="پسند")
